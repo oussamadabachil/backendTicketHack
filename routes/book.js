08df9710e3693bdec8724ece30ce4f1a37e4d310
OussamaDabachil <oussamadabachilpro@gmail.com>
2023-01-28 21:57:26 +0100

    add mongoose

--- routes/book.js
+++ routes/book.js
@@ -1,27 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('../models/connexion')
-const trips = require('../models/trips')
+require("../models/connexion");
+const trips = require("../models/trips");
 
-let arrayTrips =  [];
+let arrayTrips = [];
 
+router.post("/showTrip", (req, res) => {
+  if (arrayTrips.includes(req.body.idTrip)) {
+    res.json("you already booked this trip");
+  } else {
+    res.json("you booked this trip");
+    arrayTrips.push(req.body.idTrip);
+    console.log(arrayTrips);
+  }
+});
 
-router.post('/showTrip', (req, res)=> {
-        if(arrayTrips.includes(req.body.idTrip)){
-                res.json("you already booked this trip")
-              }
-              else{
-                res.json("you booked this trip")
-                arrayTrips.push(req.body.idTrip)
-                console.log(arrayTrips)
-              }
-           
-     })
-    
-router.get('/showTrip', (req, res)=> {
-  trips.find({_id:{$in:arrayTrips}}).then(data=>{
-        res.json(data)
-         })
-    })
+router.get("/showTrip", (req, res) => {
+  trips.find({ _id: { $in: arrayTrips } }).then((data) => {
+    res.json(data);
+  });
+});
 
 module.exports = router;
